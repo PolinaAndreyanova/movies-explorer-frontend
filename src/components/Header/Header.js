b985437c2bd4b57f8c046f1012af0556a842648e
Polina Andreyanova <andreyanovalina@yandex.ru>
2023-01-31 19:31:12 +0300

    feat: поправить Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,27 +3,40 @@ import './Header.css';
 import headerLogoPath from '../../images/logo.svg';
 import headerAccountIconPath from '../../images/header-account-icon.svg';
 
-import { NavLink, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Header(props) {
   return (
-    <header className={`header${props.loggedIn ? ' header__type_login' : ''}`} style={!props.loggedIn ? { backgroundColor: "#F3C1F8" } : { backgroundColor: "white" }}>
+    <header className='header' style={!props.loggedIn ? { backgroundColor: "#F3C1F8" } : { backgroundColor: "white" }}>
       <img src={headerLogoPath} alt="Логотип" className="header__logo" />
       <div className='header__navigation'>
-        {!props.loggedIn ?
-          <>
-            <Link to='/signup' className='header__registration-button'>Регистрация</Link>
-            <Link to='/signin' className='header__enter-button'>Войти</Link>
-          </> :
-          <>
-            <div className='header__films-navigation'>
-              <NavLink to="/movies" className='header__films-button'>Фильмы</NavLink>
-              <NavLink to='/saved-movies' className='header__saved-films-button'>Сохранённые фильмы</NavLink>
-            </div>
-            <Link to='/profile' className='header__account-button'>Аккаунт</Link>
-            <img src={headerAccountIconPath} alt='Иконка' className='header__account-icon' />
-          </>
-        }
+        <div className='header__film-pages-navigation'>
+          {props.loggedIn &&
+            <>
+              <Link to="/movies" className='header__films-button'>Фильмы</Link>
+              <Link to='/saved-movies' className='header__saved-films-button'>Сохранённые фильмы</Link>
+            </>
+          }
+        </div>
+        <div className='header__user-pages-navigation'>
+          {props.loggedIn ?
+            <>
+              <div className='header__account-link'>
+                <Link to='/profile' className='header__account-link-button'>Аккаунт</Link>
+                <img src={headerAccountIconPath} alt='Иконка' className='header__account-link-icon' />
+              </div>
+              <div className="header__hamburger-lines">
+                <span className="header__hamburger-line"></span>
+                <span className="header__hamburger-line"></span>
+                <span className="header__hamburger-line"></span>
+              </div>
+            </> :
+            <>
+              <Link to='/signup' className='header__registration-button'>Регистрация</Link>
+              <Link to='/signin' className='header__enter-button'>Войти</Link>
+            </>
+          }
+        </div>
       </div>
     </header>
   );
